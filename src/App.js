8de92a1a252e4fa0fc5ef749c94c6ed579902e6e
shaannessy25 <shaan.hurley25@gmail.com>
2020-10-26 01:17:18 -0700

    adding utils, store, and provider

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './reducers'
+
 import GridBoard from './components/GridBoard'
 import NextBlock from './components/NextBlock'
 import ScoreBoard from './components/ScoreBoard'
@@ -6,18 +10,23 @@ import Controls from './components/Controls'
 import MessagePopUp from './components/MessagePopup'
 import './App.css';
 
+
+const store = createStore(reducers)
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Tetris Redux</h1>
-      </header> 
-      <GridBoard /> 
-      <NextBlock /> 
-      <ScoreBoard />
-      <Controls />
-      <MessagePopUp />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Tetris Redux</h1>
+        </header> 
+        <GridBoard /> 
+        <NextBlock /> 
+        <ScoreBoard />
+        <Controls />
+        <MessagePopUp />
+      </div>
+    </Provider>
   );
 }
 
